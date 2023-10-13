8dbc6df5bc80192232277e6fa99ed5f1b651a356
Marcos Lucas Vieira <102480205+MarcosLucasVieira@users.noreply.github.com>
2023-10-13 16:07:01 -0300

    UserRepository

--- todolist/src/main/java/br/com/marcoslucas/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/marcoslucas/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.marcoslucas.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,25 +12,27 @@ import org.springframework.web.bind.annotation.RestController;
  * MOD
  * 
  */
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    /*
-     * String(texto)
-     * integer(int) numeros inteiros
-     * double (double) numeros 0.000
-     * float(float) numeros 0.0000
-     * char(A C)
-     * data(data)
-     * void 
-     */
-    /*
-    *body  
-    */
+
+    @Autowired
+    private IUserRepository userRepository;
 
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getUsername());
+    public ResponseEntity  create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        
+        if(user !=null){
+            System.out.println("Usuário Já Existe");
+            //mensagem de Erro
+            //status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário Já existe");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
     
 }
